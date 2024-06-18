ef85485969d51cbb9ff95a02cf49a2dc40fd9f9f
Brenden Hartman <brendenhart22@gmail.com>
2024-06-18 14:43:18 -0500

    work session one on models and beginning app resources

--- server/app.py
+++ server/app.py
@@ -9,13 +9,50 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import Car, CarMeet, Driver, Spot
 
 # Views go here!
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+class Cars(Resource):
+    
+    def get(self):
+        pass
+    def post(self):
+        pass
+    def patch(self):
+        pass
+    def delete(self):
+        pass
+
+class Drivers(Resource):
+    
+    def get(self):
+        pass
+    def post(self):
+        pass
+    def patch(self):
+        pass
+    def delete(self):
+        pass
+
+class Meets(Resource):
+    
+    def get(self):
+        pass
+    def post(self):
+        pass
+    def patch(self):
+        pass
+    def delete(self):
+        pass
+
+api.add_resource(Drivers, '/drivers')
+api.add_resource(Cars,'/cars')
+api.add_resource(Meets, '/meets')
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -2,5 +2,84 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
+#==========================================================================
 
-# Models go here!
+
+class Car(db.Model,SerializerMixin):
+    __tablename__ = 'cars'
+
+    serialize_rules = ('-driver.cars',)
+
+    id = db.Column(db.Integer, primary_key = True)
+    make = db.Column(db.String)
+    model = db.Column(db.String)
+    type = db.Column(db.String)
+
+    driver_id = db.Column(db.Integer, db.ForeignKey('drivers.id'))
+    driver = db.Relationship('Driver', back_populates = 'cars')
+
+    def __repr__(self):
+        return f'<Car {self.id}, {self.make}, {self.model}, {self.type}>'
+
+        
+#==========================================================================
+
+
+class CarMeet(db.Model,SerializerMixin):
+    __tablename__ = 'car_meets'
+
+    serialize_rules = ('-drivers.car_meets', '-spots.car_meet')
+
+    id = db.Column(db.Integer, primary_key = True)
+    type = db.Column(db.String)
+    start_date  = db.Column(db.String)
+    end_date = db.Column(db.String)
+
+    drivers = association_proxy('spots', 'driver', creator= lambda driver_obj: Spot(driver  = driver_obj))
+
+    spots = db.Relationship('Spot', back_populates = 'car_meet')
+
+    def __repr__(self):
+        return f'<Meet {self.id}, {self.type}, {self.start_date} to {self.end_date}>'
+
+
+#==========================================================================
+
+
+class Spot(db.Model, SerializerMixin):
+    __tablename__ = 'spots'
+
+    serialize_rules = ('-car_meet.spots', '-driver.spots')
+
+    id = db.Column(db.Integer, primary_key = True)
+    grade = db.Column(db.Integer)
+    reserved = db.Column(db.Boolean)
+
+    driver_id = db.Column(db.Integer, db.ForeignKey('drivers.id'))
+    driver =  db.Relationship('Driver', back_populates = 'spots')
+
+    car_meet_id = db.Column(db.Integer, db.ForeignKey('car_meets.id'))
+    car_meet = db.Relationship('CarMeet', back_populates = 'spots')
+
+    def __repr__(self):
+        return f'<Spot {self.id}, {self.grade}, {self.driver_id}, {self.car_meet_id}>'
+
+
+#==========================================================================
+
+
+class Driver(db.Model,SerializerMixin):
+    __tablename__ = 'drivers'
+
+    serialize_rules = ('-cars.driver', '-car_meets.drivers', '-spots.driver')
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    color = db.Column(db.String)
+
+    cars = db.Relationship('Car', back_populates = 'driver')
+    car_meets = association_proxy('spots', 'car_meet', creator= lambda meet_obj: Spot(meet  = meet_obj))
+    spots = db.Relationship('Spot', back_populates = 'driver')
+    
+    def __repr__(self):
+        return f'<Driver {self.id}, {self.name}, {self.color}>'
