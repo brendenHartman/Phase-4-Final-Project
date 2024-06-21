24139a7eab0c77ebb669d46d31be19e334a7a4ef
Brenden Hartman <brendenhart22@gmail.com>
2024-06-21 02:07:46 -0500

    started the structure  for my client side

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import App from "./components/App";
-import "./index.css";
+import "./Index.css";
+import { RouterProvider } from "react-router-dom"
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<RouterProvider router={router}/>);
---
+++ client/src/router.js
@@ -0,0 +1,49 @@
+
+import { createBrowserRouter } from "react-router-dom"
+import App from './components/App'
+import Home from './components/Home'
+import Drivers from './components/Drivers'
+import Cars from './components/Cars'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Meets from './components/Meets'
+
+const router = createBrowserRouter([{
+    path: '/',
+    element: <App/>,
+    errorElement: <Error />,
+    children: [
+        {
+            path: '/',
+            element: <Home/>,
+            errorElement: <Error />
+        },
+        {
+            path: '/my_driver',
+            element: <Drivers/>,
+            errorElement: <Error />
+        },
+        {
+            path: '/cars',
+            element: <Cars/>,
+            errorElement: <Error />
+        },
+        {
+            path: '/meets',
+            element: <Meets/>,
+            errorElement: <Error />
+        },
+        {
+            path: '/login',
+            element: <Login/>,
+            errorElement: <Error />
+        },
+        {
+            path: '/sign_up',
+            element: <Signup/>,
+            errorElement: <Error />
+        },
+    ]
+}])
+
+export default router
