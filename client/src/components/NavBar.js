cc8247b3bde2c27418daad7731534cd95332647e
Brenden Hartman <brendenhart22@gmail.com>
2024-09-09 23:58:10 -0500

    finished signout debugging and worked on site theme

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 
 function NavBar({user, handleClick}){
     return(
-        <div className="navBar">
+        <div className="navBar" id='navBar' style={{background: user.color}}>
             <input id="menu button"/>
+            <div id='color' style={{color: user.color}}></div>
             <div id="LinkContainer">
                 <Link className="nav-link" to="/">Profile</Link>
                 <Link className="nav-link" to="/cars">Cars</Link>
                 <Link className="nav-link" to="/meets">Meets</Link>
             </div>
-            <input id="signoutButton" type="button" onClick={handleClick}/>
+            <input id="signoutButton" type="button" value='Signout' onClick={handleClick}/>
         </div>
     );
 }
