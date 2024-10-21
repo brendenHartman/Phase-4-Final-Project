b8569aff0337293682d2ac92816ae1606514984e
Brenden Hartman <brendenhart22@gmail.com>
2024-10-21 01:38:30 -0500

    finished workiing on login screen

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -11,15 +11,21 @@ function Login({formik, formik2}){
     
     return (
         <div id='loginScreen'>
+          <div id='loginBackground'></div>
           <h1 id='loginTitle'>Welcome to Aqua's Car Company</h1>
+          <div class='loginDivider'></div>
           <form id="signupForm" onSubmit={onSubmit}>
             <h4 id='signUpTitle'>SignUp Here</h4>
+            <label htmlFor='email'>Email:</label>
             <input id="email" className='signInput' onChange={handleChange} value={values.email} />
             <p>{formik.errors.email}</p>
+            <label htmlFor='username'>Username:</label>
             <input id="username" className='signInput' onChange={handleChange} value={values.username} />
             <p>{formik.errors.username}</p>
+            <label htmlFor='password'>Password:</label>
             <input id="password" className='signInput' onChange={handleChange} value={values.password} />
             <p>{formik.errors.password}</p>
+            <label id='colorLabel' htmlFor='color'>Account Color:</label>
             <select id='color' onChange={handleChange} value={values.color}>
                     <option value='White' label='White'>White</option>
                     <option value='Red' label='Red'>Red</option>
@@ -36,10 +42,13 @@ function Login({formik, formik2}){
             <p>{formik.errors.color}</p>
             <input id="submit" type="submit" />
           </form> 
+          <div class='loginDivider'></div>
           <h4 id='loginNotice'>Already Have An Account? Login Below!</h4>
           <form id='loginForm' onSubmit={onSubmit2}>
+            <label htmlFor='username'>Username:</label>
             <input id="username" className='logInput' onChange={handleChange2} value={values2.username} />
             <p>{formik2.errors.username}</p>
+            <label htmlFor='password'>Password:</label>
             <input id="password" className='logInput' onChange={handleChange2} value={values2.password} />
             <p>{formik2.errors.password}</p>
             <input id="submit" type="submit" />
